9163234d5f4e71a67c8a30c3fc2bfa3d0cc6c853
JamesZ <jiayang_zhao@ucsb.edu>
2021-04-02 16:37:59 +0800

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+// helloWorld.cpp  Z. Wood for UCSB CS32 W21
+// Edited by: Jiayang Zhao
+// minimal Hello World! program for testing Makefiles
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
